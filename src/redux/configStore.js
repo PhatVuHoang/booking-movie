8794c10e7fafb40b69ac81db9e93c1dcd3768846
Vu Hoang Phat <phatvh.work@gmail.com>
2021-07-10 15:11:36 +0700

    20210710

--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -1,9 +1,10 @@
-import {combineReducers, createStore} from 'redux';
+import {combineReducers, createStore, applyMiddleware} from 'redux';
+import reduxThunk from 'redux-thunk';
 
 const rootReducer = combineReducers({
     //reducer khai báo tại đây
 })
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(reduxThunk));
 
 export default store;
