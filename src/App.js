9371d78b9adef02b75d104eada6eb0c4ee4611b5
Vu Hoang Phat <phatvh.work@gmail.com>
2021-07-10 14:48:02 +0700

    commit 20210710

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>làm theo trang web này <a href="https://tix.vn/" target="_blank">tix.vn</a></p>
+    </div>
+  );
+}
+
+export default App;
